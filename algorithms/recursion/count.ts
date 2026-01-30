0b215948574dd5e8ca2a8b5187b4a0efdb7a420e
juliaam <juliademoraess@gmail.com>
2026-01-29 22:23:16 -0300

    only the ones i know

--- algorithms/recursion/count.ts
+++
@@ -1,8 +0,0 @@
-export const count = (list: any[]) => {
-  if (list.length === 0) return null
-  if (list.length === 1) return 1;
-
-  return 1 + count(list.slice(0, -1))
-  
-}
-console.log(count([]))
--- algorithms/recursion/higher.ts
+++
@@ -1,6 +0,0 @@
-export const higher = (list: number[]) => {
-  if (list.length === 0) return null
-  if (list.length === 2) return list[0] > list[1] ? list[0] : list[1]
-
-  
-}
